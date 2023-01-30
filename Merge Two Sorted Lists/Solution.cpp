81cc2c09389be4b0ba2c2a7828721ebb2160339f
snzisad <snzisad@gmail.com>
2023-01-30 04:59:37 +0100

    Merge two sorted lists

---
+++ Merge Two Sorted Lists/Solution.cpp
@@ -0,0 +1,45 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == nullptr) return list2;
+        if(list2 == nullptr) return list1;
+
+        ListNode* list3 = new ListNode();
+        ListNode* head = list3;
+
+        while(list1 != nullptr && list2 != nullptr){
+            if(list1->val < list2->val){
+                list3->next = new ListNode(list1->val);
+                list1 = list1->next;
+            }
+            else{
+                list3->next = new ListNode(list2->val);
+                list2 = list2->next;
+            }
+            list3 = list3->next;
+        }
+
+        while(list1 != nullptr){
+            list3->next = new ListNode(list1->val);
+            list1 = list1->next;
+            list3 = list3->next;
+        }
+
+        while(list2 != nullptr){
+            list3->next = new ListNode(list2->val);
+            list2 = list2->next;
+            list3 = list3->next;
+        }
+
+        return head->next;
+
+    }
+};
