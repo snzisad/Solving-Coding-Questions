fa6eb1e4d7c7b395aa5699bc3f67c00d80d92c72
snzisad <snzisad@gmail.com>
2023-01-31 02:32:12 +0100

    Remove Element using cpp

---
+++ Remove Element/Solution.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k += 1;
+            }
+        }
+
+        return k;
+    }
+};
